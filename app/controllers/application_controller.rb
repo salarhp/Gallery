cedde6012e88ffedadc815d7432cab11376b6fb4
salarhp <salarhoseinpuor@gmail.com>
2018-09-08 11:26:28 +0430

    modified application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :getCategoryNav
+    protect_from_forgery with: :exception
+
+    private
+    def getCategoryNav
+        @getCategoryNav= Category.all
+    end
 end
